a195e5575c5588a614e39a95cc83bff944da5dba
GuilhermesonStorm <67202219+GuilhermesonStorm@users.noreply.github.com>
2021-08-05 18:04:31 -0300

    Add files via upload

---
+++ Exercicio1016/Exercicio1016/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Exercicio1016
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int distancia, tempo;
+            Console.WriteLine("Entre com uma distância em km: ...");
+            distancia = int.Parse(Console.ReadLine());
+            tempo = distancia * 2;
+            Console.WriteLine($"O tempo para o carro Y tomar distância é: {tempo} minutos.");
+        }
+    }
+}
