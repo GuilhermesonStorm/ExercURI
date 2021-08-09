e6bb674d1194e90ae639ec67a3f3c57e06af56f6
GuilhermesonStorm <67202219+GuilhermesonStorm@users.noreply.github.com>
2021-08-09 17:41:18 -0300

    Add files via upload

---
+++ Exercicio1035/Exercicio1035/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Exercicio1035
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B, C, D;
+
+            Console.WriteLine("Entre com um valor para A: ...");
+            A = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com um valor para B: ...");
+            B = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com um valor para C: ...");
+            C = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com um valor para D: ...");
+            D = int.Parse(Console.ReadLine());
+
+            if (B > C && D > A && (C + D) > (A + B) && C > 0 && D > 0 && A % 2 == 0)
+            {
+                Console.WriteLine("Valores aceitos!");
+            }
+            else
+            {
+                Console.WriteLine("Valores não aceitos.");
+            }
+        }
+    }
+}
